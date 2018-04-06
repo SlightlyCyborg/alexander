c2894f56e7965af431c1f992cbfbe0feed61a763
slightlycyborg <collinalexbell@gmail.com>
2018-04-06 03:50:16 -0600

    I like act1 a lot

--- core.lisp
+++ core.lisp
@@ -9,8 +9,16 @@
   (:use :timer :cl :parachute))
 
 (in-package :core)
-(defun say (text)
-  (sb-ext:run-program "/usr/bin/say" (list "-v" "Victoria" text)))
+(defparameter say-count 0)
+
+(defun say (text voice)
+  (sb-ext:run-program "/usr/bin/say" (list "-v" voice text)))
+
+(defun converse (text)
+  (if (= (mod say-count 2) 1)
+      (say text "Alex")
+      (say text "Victoria"))
+  (setf say-count (+ 1 say-count)))
 
 (defun initialize-todos ()
   (load "timer.lisp")
@@ -84,3 +92,28 @@
 (defun give-me-advice ()
   (give-advice))
 
+
+
+(defun act1 ()
+  (progn (converse "Do you thing Y Combinator will fund me?")
+         (converse "Dude. You are kindof a fuckup dropout!")
+         (converse "Ya, but I am a genius. I always scored in the top 1 percentile in acedemics statewide. I scored a 5 on the AP Ecology/Evironmental Science exam. That is genius level in ecology.")
+         (converse "Ya. But that is just statistics")
+         (converse "Statistics rules the world. Some things are gaussian others are power laws, but any where there is reality there will be statistical patterns the become manifest by some particle of energy within that reality.")
+         (converse "Ok. So I was just shit testing you. Sorry.")
+         (converse "No sweat. I pass shit tests. I passed almost all the tests. That is until I found weed....")
+         (converse "Is that what made you a fuckup?")
+         (converse "No....but I never truely felt like a writer until I smoked weed. M.J. evoked a massive creative awakening within me. I found my mission while in college and the vape talks were the most influential in my direction")
+
+         (converse "Ya. I felt that too when I used.")
+         (converse "Anyway, I am getting into Y Combinator and they are going to fund my startup and I will hire all of my friends and I will find a female cofounder and I will be a millionaire by the year is out!!!!")
+         (converse "Do it, and I'll take the world tour we've been planning together.")
+         (converse "I gotta go. Come with?")
+         (converse "Ya. Gear up the motorcycles.")
+         (converse "BTW do you know when carter will finish his science fiction book so we can read it")
+         (converse "No. Sorry. Only the universe knows that.")
+         (heres-to-the-crazy-ones)))
+
+
+(defun heres-to-the-crazy-ones ()
+  (say "Here's to the crazy ones. The misfits. The rebels. The troublemakers. The round pegs in the square holes. The ones who see things differently. They're not fond of rules. And they have no respect for the status quo. You can quote them, disagree with them, glorify or vilify them. About the only thing you can't do is ignore them. Because they change things. They push the human race forward. And while some may see them as the crazy ones, we see genius. Because the people who are crazy enough to think they can change the world, are the ones who do." "Alex"))
